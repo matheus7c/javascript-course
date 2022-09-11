ccd3bb81dc40e68e1daec5c5ba5553adacfb78f9
Matheus Castro do Nascimento <46095722+matheus7c@users.noreply.github.com>
2022-09-11 17:28:01 -0300

    Criando Regex e tratando

--- nodejs-criando-sua-biblioteca/index.js
+++ nodejs-criando-sua-biblioteca/index.js
@@ -1,6 +1,16 @@
 const chalk = require("chalk");
 const fs = require("fs");
 
+function extraiLinks(texto) {
+  const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+  const arrayResultados = [];
+  let temp;
+  while ((temp = regex.exec(texto)) !== null) {
+    arrayResultados.push({ [temp[1]]: temp[2] });
+  }
+  return arrayResultados;
+}
+
 function trataErro(erro) {
   throw new Error(chalk.red(erro));
 }
@@ -29,7 +39,7 @@ async function pegaArquivoAwait(caminhoDoArquivo) {
   const encoding = "utf-8";
   try {
     const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-    console.log(chalk.yellow(texto));
+    console.log(extraiLinks(texto));
   } catch (erro) {
     trataErro(erro);
   }
